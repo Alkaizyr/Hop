4c277a5bb7be34e23e84130a5f889785b47154ce
Anton Ostrouhhov <a.ostrouhhov@gmail.com>
2018-04-19 01:14:57 +0300

    Add CoordinatorLayout and icons

--- app/src/main/java/com/hop/BeerAdapter.kt
+++ app/src/main/java/com/hop/BeerAdapter.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.beer_list_item.view.*
 
 class BeerAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
-    // Gets the number of animals in the list
+    // Gets the number of beers in the list
     override fun getItemCount(): Int {
         return items.size
     }
@@ -19,13 +19,13 @@ class BeerAdapter(val items : ArrayList<String>, val context: Context) : Recycle
         return ViewHolder(LayoutInflater.from(context).inflate(R.layout.beer_list_item, parent, false))
     }
 
-    // Binds each animal in the ArrayList to a view
+    // Binds each beer in the ArrayList to a view
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.tvAnimalType?.text = items.get(position)
+        holder?.tvBeerName?.text = items.get(position)
     }
 }
 
 class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-    // Holds the TextView that will add each animal to
-    val tvAnimalType = view.tv_beer_name
+    // Holds the TextView that will add each beer to
+    val tvBeerName = view.tv_beer_name
 }
--- app/src/main/java/com/hop/MainActivity.kt
+++ app/src/main/java/com/hop/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hop
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,25 +11,49 @@ class MainActivity : AppCompatActivity() {
     val beers: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
 
         // Loads beers into the ArrayList
         addBeers()
 
-        // Creates a vertical Layout Manager
+//         Creates a vertical Layout Manager
         rv_beer_list.layoutManager = LinearLayoutManager(this)
 
         // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
 //        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
 
-        // Access the RecyclerView Adapter and load the data into it
+//         Access the RecyclerView Adapter and load the data into it
         rv_beer_list.adapter = BeerAdapter(beers, this)
 
     }
 
     // Adds beers to the empty animals ArrayList
     fun addBeers() {
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
+        beers.add("8 Balls")
+        beers.add("Friday Night")
         beers.add("8 Balls")
         beers.add("Friday Night")
     }
