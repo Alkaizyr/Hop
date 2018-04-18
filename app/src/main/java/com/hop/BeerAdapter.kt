0c8603a65844be305fcedc0780e1e4e14750968e
Anton Ostrouhhov <a.ostrouhhov@gmail.com>
2018-04-19 02:20:19 +0300

    Add adapter_beer.xml

--- app/src/main/java/com/hop/BeerAdapter.kt
+++ app/src/main/java/com/hop/BeerAdapter.kt
@@ -5,7 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.beer_list_item.view.*
+import kotlinx.android.synthetic.main.adapter_beer.view.*
 
 class BeerAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
@@ -16,16 +16,22 @@ class BeerAdapter(val items : ArrayList<String>, val context: Context) : Recycle
 
     // Inflates the item views
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.beer_list_item, parent, false))
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.adapter_beer, parent, false))
     }
 
     // Binds each beer in the ArrayList to a view
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
         holder?.tvBeerName?.text = items.get(position)
+        holder?.tvBrewery?.text = items.get(position)
+        holder?.tvCreationDate?.text = items.get(position)
+        holder?.tvBeerStyle?.text = items.get(position)
     }
 }
 
 class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     // Holds the TextView that will add each beer to
-    val tvBeerName = view.tv_beer_name
+    val tvBeerName = view.tvBeerName
+    val tvBrewery = view.tvBrewery
+    val tvCreationDate = view.tvCreationDate
+    val tvBeerStyle = view.tvBeerStyle
 }
